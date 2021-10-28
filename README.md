221836f1618e7a148bd351442f1a9af169d24740
Suraj Kumar <vermasuraj412@gmail.com>
2021-10-28 16:43:07 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Todo Web-App
+
+* build using node-express.
+* used **ejs** for templating.
+    * we can write html code inside .ejs
+    * we can also write js code by wrapping
+    it using a particuler tag(<% js %>).
+    * we must render views/html.ejs file by providing **all** of the keys present inside .ejs file
+
+**Node:** data will be lost on refreshing, since db is not used.
---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+const express = require("express");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+var todos = [];
+
+app.get("/", function(req, res) {
+
+    let date = new Date();
+
+    let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    let today = date.toLocaleDateString("en-US", options);
+
+    res.render("list", { // This assumes a views directory containing an list.ejs page.
+        typeOfDay: today,  // key(inside ejs) - value(current value) pair
+        newItem: todos
+    });
+});
+
+
+app.post("/", function(req, res) {
+    todos.push(req.body.inputTodo.toString());
+
+    // console.log(todos);
+    res.redirect("/");
+});
+
+
+app.listen(3000, function() {
+    console.log("Server live at 3000...");
+});
