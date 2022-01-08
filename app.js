8aa72a4db763040043572fb1fe088c9278f9161b
Suraj Kumar <vermasuraj412@gmail.com>
2022-01-08 19:05:20 +0530

    added database(mongoDB) to save todos locally

--- app.js
+++ app.js
@@ -1,41 +1,73 @@
 
 
 const express = require("express");
+const mongoose = require('mongoose');
 const date = require(__dirname + "/local_node_modules/date.js");
 
-
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/todoDB');
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- hit this button to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
 const todos = []; // we can modify the const array but can't reassign it to a new array
 const workTodos = [];
-
 
 app.get("/", function(req, res) {
 
     let today = date.today(); // using local module
 
-    res.render("list", { // This assumes a views directory containing an list.ejs page.
-        listTitle: today,  // key(inside ejs) - value(current value) pair
-        newItem: todos
+    Item.find({}, function(err, foundItems) {
+
+        if(foundItems.length === 0) {
+            Item.insertMany(defaultItems, function(err) {
+                if(err) console.log(err);
+                else console.log("Successfully saved to database");
+            });
+        }
+
+        res.render("list", { // This assumes a views directory containing an list.ejs page.
+            listTitle: today,  // key(inside ejs) - value(current value) pair
+            newItem: foundItems
+        });
     });
+
+    
 });
 
 
 app.post("/", function(req, res) {
 
-    if(req.body.todoSubmit == "Work") { // we are getting dynamic value using ejs.
-        workTodos.push(req.body.inputTodo.toString());
-        res.redirect("/work");
-    }
-    else {
-        todos.push(req.body.inputTodo.toString());
-        res.redirect("/");
-    }
+    const itemName = req.body.inputTodo
+    console.log(itemName);
+    const currItem = new Item({
+        name: itemName
+    });
+
+    currItem.save();
+
+    res.redirect('/');
 });
 
 
