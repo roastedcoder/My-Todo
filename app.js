81c9628d0d048992c470a366c24baaeba29716d6
Suraj Kumar <vermasuraj412@gmail.com>
2022-01-16 13:35:13 +0530

    a complete todo application build using nodejs and mongodb, deployed on heroku

--- app.js
+++ app.js
@@ -147,7 +147,10 @@ app.post("/work", function(req, res) {
 
 
 
-
-app.listen(3000, function() {
-    console.log("Server live at 3000...");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port, function() {
+    console.log("Server live");
 });
